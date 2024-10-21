fc341278775a9e6ac54266d70a3832a7410dff15
Md. Elias <elias1435@gmail.com>
2024-10-21 18:43:58 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -21,38 +21,6 @@ function serve_custom_410_page() {
             status_header(410);
             nocache_headers();
 
-            // Load the 'error-410' page template
-            include(get_template_directory() . '/410.php');
-
-            exit; // Prevent any further output
-        }
-    }
-}
-add_action('template_redirect', 'serve_custom_410_page', 0);
-
-
-
-
-function serve_custom_410_page() {
-    // List of URLs to return 410 status
-    $urls = [
-        '/example.php',
-        '/example1.php',
-        '/raucherentwoehnung.php',
-        '/example2.php',
-        '/pages/example3/'
-    ];
-
-    // Get the current request URI
-    $request_uri = $_SERVER['REQUEST_URI'];
-
-    // Check if the current request URI matches any in the list
-    foreach ($urls as $url) {
-        if (strpos($request_uri, $url) !== false) {
-            // Set 410 header
-            status_header(410);
-            nocache_headers();
-
             // Load the 'error-410' page by its ID
             $error_page_id = 5583; // Replace with the actual page ID for your 'error-410' page. this is the page created on pages just add the page id. it will show the page content...
             $error_page = get_post($error_page_id);
