f9cba55613fa357b2653df92a1722e84795e37e6
Md. Elias <elias1435@gmail.com>
2024-10-22 20:45:36 +0600

    Update 410.php

--- 410.php
+++ 410.php
@@ -11,12 +11,30 @@ nocache_headers();
 session_start();
 ob_start();
 
-$not_found_page = get_post(11139); // Replace with the ID of your 'error-410' page. if you want to fetch any worpdress page content use ths line otherwise remove this line.
+$not_found_page = get_post(5583); // Replace with the ID of your 'error-410' page. if you want to fetch any worpdress page content use ths line otherwise remove this line.
 
 get_header(); // Include the header.
 ?>
 
 <div class="error-410-page">
+    <div id="feature-image">
+		  <?php
+            // Check if the post has a featured image and display it
+            if (has_post_thumbnail($not_found_page->ID)) {
+                // Retrieve the featured image URL
+                $image_url = get_the_post_thumbnail_url($not_found_page->ID, 'full');
+                ?>
+                <div class="featured-image">
+                    <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr(get_post_meta(get_post_thumbnail_id($not_found_page->ID), '_wp_attachment_image_alt', true)); ?>" />
+                </div>
+                <?php
+            } else {
+                echo '<p>No featured image found for this page.</p>';
+            }
+            ?>
+	</div>
+
+
     <h1>Page No Longer Available</h1>
     <p>We're sorry, but the page you're looking for is no longer available. It may have been removed or moved to a new location.</p>
     
